cb6a46269fcb8e7268ff52d68dc46ac6bcf207dc
Harshini <105817423+crypte-1@users.noreply.github.com>
2022-06-24 20:45:56 +0530

    Update Greeter.sol

--- Whitelist-Dapp/hardhat-tutorial/contracts/Greeter.sol
+++ Whitelist-Dapp/hardhat-tutorial/contracts/Greeter.sol
@@ -4,16 +4,4 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Greeter {
-    string private greeting;
-
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
-    }
-
-    function setGreeting(string memory _greeting) public {
-    }
 }
