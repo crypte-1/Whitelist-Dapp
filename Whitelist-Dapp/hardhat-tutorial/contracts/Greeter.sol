104013afbb5dc176cbb093fedc6e41d5e6b78d0b
Harshini <105817423+crypte-1@users.noreply.github.com>
2022-06-24 20:45:31 +0530

    Update Greeter.sol

--- Whitelist-Dapp/hardhat-tutorial/contracts/Greeter.sol
+++ Whitelist-Dapp/hardhat-tutorial/contracts/Greeter.sol
@@ -12,11 +12,8 @@ contract Greeter {
     }
 
     function greet() public view returns (string memory) {
-        return greeting;
     }
 
     function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
     }
 }
